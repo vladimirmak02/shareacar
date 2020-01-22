3d03b0f4b67192430060b927385641887fcb2932
Vladimir <vovamak107@gmail.com>
2020-01-22 19:27:49 +0100

    Removed mentions of my name

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
 <footer>
-    <p style="text-align: left; padding-left: 5%; padding-top: 12px;">Made By Vladimir Makarov</p>
+    <p style="text-align: left; padding-left: 5%; padding-top: 12px;">Share a car</p>
 </footer>
