360cbadc1b65df17e8f1668d97e4dc0c4d2206db
Vladimir <vovamak107@gmail.com>
2020-02-24 19:07:12 +0100

    Last polish

--- index.php
+++ index.php
@@ -3,13 +3,7 @@ include("includes/a_config.php");
 session_start();
 ?>
 <!DOCTYPE html>
-<!--RESOURCES:
-https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php
-https://www.tutorialrepublic.com/php-tutorial/php-form-handling.php
-https://getbootstrap.com/docs/4.3/components/forms/
-https://www.w3schools.com/php/php_filter_advanced.asp
-YOUTUBE TUTORIAL ON SIGNUP & LOGIN
--->
+
 <html>
 <head>
     <? include("includes/head-tag-contents.php"); ?>
